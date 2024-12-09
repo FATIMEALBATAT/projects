0f52a9b230b445f6cf68ceb2fc0488e064361125
FATIMEALBATAT <93888754+FATIMEALBATAT@users.noreply.github.com>
2024-12-09 01:53:56 +0100

    Delete config directory

--- config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('BASE_URL', 'http://localhost/todoapp/');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'todo_app');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-?>
